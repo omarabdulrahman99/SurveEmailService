a666325d06af04e36010dfd1267e29784fdf87ea
sniperkills90 <saintnicks78@hotmail.com>
2018-06-05 19:43:40 -0400

    keys

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+	googleClientID: '686026913833-t96p3u9njrt9cvrvmc1578rvb6h1traq.apps.googleusercontent.com',
+	googleClientSecret:'xa5yfdED0pmZghr3N-Tch9hl',
+	mongoURI: 'mongodb://saintnicks78:cutekitteN99@ds149960.mlab.com:49960/surveyemailer'
+
+};
