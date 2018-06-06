eeb6ddefdeef3377399fc6a88d6b5c00312feae7
sniperkills90 <saintnicks78@hotmail.com>
2018-06-06 17:47:54 -0400

    react starter

--- services/passport.js
+++ services/passport.js
@@ -25,9 +25,11 @@ passport.deserializeUser((id, done) => { //turn id to user model instance
 
 passport.use(new GoogleStrategy({
 
-	clientID: keys.googleClientID,
+	clientID: keys.googleClientID, //these to connect to google form login api
 	clientSecret: keys.googleClientSecret,
-	callbackURL:'/auth/google/callback'
+	callbackURL:'/auth/google/callback',
+	proxy: true //google strategy, since this is relative path, if we specified https, itll assume http and cause error.
+	//you can specify https here, full domain. or specify proxy:true, since googlestrategy doesn't let https return through proxy
 
   }, (accessToken, refreshToken, profile, done) => {
 
