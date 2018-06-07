9e162929f1c24a6062239a7a148b5e96bc752bb4
sniperkills90 <saintnicks78@hotmail.com>
2018-06-07 18:52:48 -0400

    redux react routing setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,20 @@
+//react-redux, react-dom, react-router, react-router-dom, redux, react
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import App from './components/App';
+import reducers from './reducers' ;
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(reducers, {}, applyMiddleware());
+
+ReactDOM.render(
+
+	<Provider store={store}> 
+		<App />
+	</Provider>,
+	document.querySelector('#root')
+
+	);
+
+//provider is a react component that reads changes from redux store and informs all children components
