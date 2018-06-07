55dcfb9311b790b3b5eab179cad1386aa50833a1
omarabdulrahman99 <saintnicks78@hotmail.com>
2018-06-06 23:06:03 -0400

    async

--- services/passport.js
+++ services/passport.js
@@ -1,54 +1,37 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
+const keys = require('../config/keys');
 
-
-
-const keys = require('../config/keys.js');
 const User = mongoose.model('users');
 
-passport.serializeUser((user,done) =>{ //turn user model instance to an ID
-
-	done(null, user.id);//first argument is an error object.
-
+passport.serializeUser((user, done) => {
+  done(null, user.id);
 });
 
-passport.deserializeUser((id, done) => { //turn id to user model instance
-
+passport.deserializeUser((id, done) => {
   User.findById(id).then(user => {
-
     done(null, user);
-
-  })
-
+  });
 });
 
-passport.use(new GoogleStrategy({
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+      proxy: true
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
 
-	clientID: keys.googleClientID, //these to connect to google form login api
-	clientSecret: keys.googleClientSecret,
-	callbackURL:'/auth/google/callback',
-	proxy: true //google strategy, since this is relative path, if we specified https, itll assume http and cause error.
-	//you can specify https here, full domain. or specify proxy:true, since googlestrategy doesn't let https return through proxy
-
-  }, (accessToken, refreshToken, profile, done) => {
-
-  		User.findOne({ googleId: profile.id })//cant set var user = to this result bc this returns a promise.
-  			.then((existingUser) => {
-  				if(existingUser){
-
-  					done(null, existingUser);
-
-  				}else{
-
-  					new User({googleId: profile.id}).save()
-  							.then(user => done(null, user));
-  				}
-
-  			})
-  		
-
-  })
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+      
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
+    }
+  )
 );
-
-
