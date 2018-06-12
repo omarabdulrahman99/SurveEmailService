cdeeeaffb1a689d4d1c92025f05d091a8ebc004d
omarabdulrahman99 <saintnicks78@hotmail.com>
2018-06-11 23:09:13 -0400

    auth middleware

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,11 +1,30 @@
 const stripe = require('stripe')('sk_test_jhtAOziCfFtzXcrK0e0r5JJn');
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
 
-	app.post('/api/stripe', (req,res) => {
+	app.post('/api/stripe', requireLogin, async (req,res) => {
 
 
+		
+
+		const charge = await stripe.charges.create({
+
+			amount:500,
+			currency: 'usd',
+			description:'$5 for 5 credits',
+			source: req.body.id
+
+		})		
+
+
+		req.user.credits += 5;
+		const user = await req.user.save();
+
+		res.send(user);
 
 	});
 
 };
+
+
